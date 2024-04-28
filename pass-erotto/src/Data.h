1c86e3ceea742a56b897cfb4b00e282f2a3d68df
AlenDio0 <alendio.0000@gmail.com>
2024-04-28 23:17:21 +0200

    Add FPSLIMIT and VSYNC
    Added the two static const in Data.h and initialized into the window int Data.cpp

--- pass-erotto/src/Data.h
+++ pass-erotto/src/Data.h
@@ -9,6 +9,8 @@ namespace Data
 	static const unsigned int WINDOW_WIDTH = 200;
 	static const unsigned int WINDOW_HEIGTH = 800;
 	static const sf::Uint32 WINDOW_STYLE = sf::Style::Close;
+	static const unsigned int WINDOW_FPSLIMIT = 60;
+	static const bool WINDOW_VSYNC = false;
 
 	static sf::RenderWindow* s_Window = NULL;
 	static StateMachine s_Machine;
