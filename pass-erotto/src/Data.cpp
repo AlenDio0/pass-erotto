4c7afb4faf6b186bcc959ce72cd27b8ca4272f0c
AlenDio0 <alendio.0000@gmail.com>
2024-04-29 16:01:35 +0200

    Add WINDOW_FONT
    Created a default font
    TO-DO: Assets file

--- pass-erotto/src/Data.cpp
+++ pass-erotto/src/Data.cpp
@@ -4,6 +4,9 @@ namespace Data
 {
 	void INIT()
 	{
+		WINDOW_FONT = new sf::Font();
+		WINDOW_FONT->loadFromFile("C:/Windows/Fonts/arial.ttf");
+
 		s_Window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGTH), WINDOW_TITLE, WINDOW_STYLE);
 
 		s_Window->setFramerateLimit(WINDOW_FPSLIMIT);
--- pass-erotto/src/Data.h
+++ pass-erotto/src/Data.h
@@ -6,11 +6,14 @@ namespace Data
 {
 	//TO DO: ini parser
 	static const std::string WINDOW_TITLE = "Pass-erotto";
-	static const unsigned int WINDOW_WIDTH = 200;
+	static const unsigned int WINDOW_WIDTH = 400;
 	static const unsigned int WINDOW_HEIGTH = 800;
 	static const sf::Uint32 WINDOW_STYLE = sf::Style::Close;
 	static const unsigned int WINDOW_FPSLIMIT = 60;
 	static const bool WINDOW_VSYNC = false;
+
+	static const sf::Color WINDOW_BACKGROUND(70, 100, 170);
+	static sf::Font* WINDOW_FONT = NULL;
 
 	static sf::RenderWindow* s_Window = NULL;
 	static StateMachine s_Machine;
