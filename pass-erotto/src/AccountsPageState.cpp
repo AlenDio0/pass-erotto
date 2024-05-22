25e09c120c182f580b69b85ef4fe3ec2efe99d05
AlenDio0 <alendio.0000@gmail.com>
2024-05-22 20:34:54 +0200

    Add on<Event> functions
    Added more private functions so that the code is more readable
    (forgot about this class)

--- pass-erotto/src/AccountsPageState.cpp
+++ pass-erotto/src/AccountsPageState.cpp
@@ -5,6 +5,8 @@
 
 using namespace Data;
 
+#define ACCOUNT_XPOS(size) (WINDOW_WIDTH / 2.f - size / 2.f)
+
 AccountsPageState::AccountsPageState()
 {
 	const float X_AXISPOS = WINDOW_WIDTH / 2.f;
@@ -44,7 +46,7 @@ void AccountsPageState::init()
 
 	for (uint16_t i = 0; i < m_Accounts.size(); i++)
 	{
-		m_Accounts[i].setPosition({ 10.f, m_Background.getSize().y + 10.f + 110.f * i });
+		m_Accounts[i].setPosition({ ACCOUNT_XPOS(m_Accounts[i].getSize().x), m_Background.getSize().y + 10.f + 110.f * i });
 	}
 }
 
@@ -58,228 +60,16 @@ void AccountsPageState::pollEvent()
 			g_Window->close();
 			break;
 		case sf::Event::MouseMoved:
-		{
-			if (m_NotifyViewAccount.isActive())
-			{
-				TextButton& button = m_NotifyViewAccount.getButtons()[Notify_ViewAccount::OK];
-
-				if (button.isCursorOn(*g_Window))
-				{
-					button.setHighlight(true);
-				}
-				else
-				{
-					button.setHighlight(false);
-				}
-
-				break;
-			}
-
-			if (m_NotifyDeleteAccount.isActive())
-			{
-				auto& buttons = m_NotifyDeleteAccount.getButtons();
-
-				for (uint8_t i = 0; i < buttons.size(); i++)
-				{
-					TextButton& button = buttons[i];
-					if (button.isCursorOn(*g_Window))
-					{
-						button.setHighlight(true);
-					}
-					else
-					{
-						button.setHighlight(false);
-					}
-				}
-
-				break;
-			}
-
-			std::vector<TextButton*> buttons;
-
-			buttons.push_back(&m_Buttons[Button::AGGIUNGI]);
-			buttons.push_back(&m_Buttons[Button::INDIETRO]);
-
-			for (Account& acc : m_Accounts)
-			{
-				buttons.push_back(&acc.getButtonView());
-				buttons.push_back(&acc.getButtonEdit());
-				buttons.push_back(&acc.getButtonDelete());
-			}
-
-			for (TextButton* button : buttons)
-			{
-				if (button->isCursorOn(*g_Window))
-				{
-					button->setHighlight(true);
-				}
-				else
-				{
-					button->setHighlight(false);
-				}
-			}
-		}
-		break;
+			onMouseMovement();
+			break;
 		case sf::Event::MouseButtonPressed:
-			if (m_NotifyViewAccount.isActive())
-			{
-				if (m_NotifyViewAccount.getButtons()[Notify_ViewAccount::OK].isCursorOn(*g_Window))
-				{
-					m_NotifyViewAccount.setActive(false);
-				}
-
-				break;
-			}
-
-			if (m_NotifyDeleteAccount.isActive())
-			{
-				auto& buttons = m_NotifyDeleteAccount.getButtons();
-
-				for (uint8_t i = 0; i < buttons.size(); i++)
-				{
-					if (buttons[i].isCursorOn(*g_Window))
-					{
-						switch (i)
-						{
-						case Notify_DeleteAccount::CONFERMA:
-						{
-							mINI::INIStructure ini;
-							DATAFILE.read(ini);
-
-							ini.remove(m_NotifyDeleteAccount.getName());
-
-							DATAFILE.write(ini);
-
-							init();
-						}
-						break;
-						case Notify_DeleteAccount::ANNULLA:
-							break;
-						}
-
-						m_NotifyDeleteAccount.setActive(false);
-					}
-				}
-
-				break;
-			}
-
-			if (m_TextBoxSearch.isCursorOn(*g_Window))
-			{
-				m_TextBoxSearch.setSelected(true);
-			}
-			else
-			{
-				m_TextBoxSearch.setSelected(false);
-			}
-
-			for (Account* acc : m_VisibleAccounts)
-			{
-				enum : uint8_t
-				{
-					MOSTRA,
-					MODIFICA,
-					ELIMINA,
-				};
-				std::unordered_map<uint8_t, TextButton*> buttons;
-
-				buttons[MOSTRA] = &acc->getButtonView();
-				buttons[MODIFICA] = &acc->getButtonEdit();
-				buttons[ELIMINA] = &acc->getButtonDelete();
-
-				for (uint8_t i = 0; i < buttons.size(); i++)
-				{
-					if (buttons[i]->isCursorOn(*g_Window))
-					{
-						switch (i)
-						{
-						case MOSTRA:
-						{
-							m_NotifyViewAccount = Notify_ViewAccount("Account: " + acc->getAccountInfo().name);
-
-							std::stringstream contents;
-							contents << "\nNome utente:\n" << acc->getAccountInfo().username << "\n\nPassword:\n" << acc->getAccountInfo().password;
-							m_NotifyViewAccount.setContents(contents.str());
-
-							m_NotifyViewAccount.setActive(true);
-						}
-						break;
-						case MODIFICA:
-							g_Machine.add(StateRef(new EditAccountState(acc->getAccountInfo())), false);
-							break;
-						case ELIMINA:
-						{
-							m_NotifyDeleteAccount = Notify_DeleteAccount(acc->getAccountInfo().name);
-
-							std::stringstream contents;
-							contents << "\nStai per eliminare:\n" << acc->getAccountInfo().name
-								<< "\n\n\n(!) Questa operazione è irreversibile.";
-							m_NotifyDeleteAccount.setContents(contents.str());
-
-							m_NotifyDeleteAccount.setActive(true);
-						}
-						break;
-						}
-					}
-				}
-			}
-
-			for (uint8_t i = 0; i < m_Buttons.size(); i++)
-			{
-				if (m_Buttons[i].isCursorOn(*g_Window))
-				{
-					switch (i)
-					{
-					case Button::AGGIUNGI:
-						g_Machine.add(StateRef(new AddAccountState()), false);
-						break;
-					case Button::INDIETRO:
-						g_Machine.remove();
-						break;
-					}
-				}
-			}
+			onMouseButtonPressed();
 			break;
 		case sf::Event::TextEntered:
 			m_TextBoxSearch.onType(event);
 			break;
 		case sf::Event::MouseWheelScrolled:
-			if (m_VisibleAccounts.empty())
-			{
-				break;
-			}
-
-			{
-				const float DELTA = event.mouseWheelScroll.delta;
-				const float X_DEFAULT = 10.f;
-
-				const int Y_MOVE = 55;
-
-				if (DELTA > 0.f)
-				{
-					for (uint16_t i = 0; i < m_VisibleAccounts.size(); i++)
-					{
-						const float Y_DEFAULT = m_Background.getSize().y + 10.f + 110.f * i;
-
-						if (m_VisibleAccounts[i]->getPosition().y != Y_DEFAULT)
-						{
-							m_VisibleAccounts[i]->setPosition({ X_DEFAULT, m_VisibleAccounts[i]->getPosition().y + Y_MOVE });
-						}
-					}
-				}
-				else if (DELTA < 0.f)
-				{
-					for (uint16_t i = 0; i < m_VisibleAccounts.size(); i++)
-					{
-						const float Y_DEFAULT = m_Background.getSize().y + 10.f + 110.f * i;
-
-						if (m_VisibleAccounts[i]->getPosition().y <= Y_DEFAULT)
-						{
-							m_VisibleAccounts[i]->setPosition({ X_DEFAULT, m_VisibleAccounts[i]->getPosition().y - Y_MOVE });
-						}
-					}
-				}
-			}
+			onScrollPage(event);
 			break;
 		}
 	}
@@ -295,43 +85,12 @@ void AccountsPageState::update()
 		if (search != m_TextBoxSearch.getBuff())
 		{
 			search = m_TextBoxSearch.getBuff();
-
-			m_VisibleAccounts.clear();
-
 			for (char& c : search)
 			{
 				c += c < 'A' || c > 'Z' ? 0 : 32;
 			}
 
-			for (Account& acc : m_Accounts)
-			{
-				std::string name = acc.getAccountInfo().name;
-
-				if (search.length() > name.length())
-				{
-					continue;
-				}
-
-				bool found = true;
-				for (int i = 0; i < search.length(); i++)
-				{
-					if (search[i] != name[i])
-					{
-						found = false;
-						break;
-					}
-				}
-
-				if (found)
-				{
-					m_VisibleAccounts.push_back(&acc);
-				}
-			}
-
-			for (uint16_t i = 0; i < m_VisibleAccounts.size(); i++)
-			{
-				m_VisibleAccounts[i]->setPosition({ 10.f, m_Background.getSize().y + 10.f + 110.f * i });
-			}
+			onSearch(search);
 		}
 	}
 	else if (m_VisibleAccounts.size() != m_Accounts.size())
@@ -346,7 +105,7 @@ void AccountsPageState::update()
 
 		for (uint16_t i = 0; i < m_VisibleAccounts.size(); i++)
 		{
-			m_VisibleAccounts[i]->setPosition({ 10.f, m_Background.getSize().y + 10.f + 110.f * i });
+			m_VisibleAccounts[i]->setPosition({ ACCOUNT_XPOS(m_Accounts[i].getSize().x), m_Background.getSize().y + 10.f + 110.f * i });
 		}
 	}
 
@@ -383,6 +142,263 @@ void AccountsPageState::render()
 	}
 
 	g_Window->display();
+}
+
+void AccountsPageState::onMouseMovement()
+{
+	if (m_NotifyViewAccount.isActive())
+	{
+		TextButton& button = m_NotifyViewAccount.getButtons()[Notify_ViewAccount::OK];
+
+		if (button.isCursorOn(*g_Window))
+		{
+			button.setHighlight(true);
+		}
+		else
+		{
+			button.setHighlight(false);
+		}
+
+		return;
+	}
+
+	if (m_NotifyDeleteAccount.isActive())
+	{
+		auto& buttons = m_NotifyDeleteAccount.getButtons();
+
+		for (uint8_t i = 0; i < buttons.size(); i++)
+		{
+			TextButton& button = buttons[i];
+			if (button.isCursorOn(*g_Window))
+			{
+				button.setHighlight(true);
+			}
+			else
+			{
+				button.setHighlight(false);
+			}
+		}
+
+		return;
+	}
+
+	std::vector<TextButton*> buttons;
+
+	buttons.push_back(&m_Buttons[Button::AGGIUNGI]);
+	buttons.push_back(&m_Buttons[Button::INDIETRO]);
+
+	for (Account& acc : m_Accounts)
+	{
+		buttons.push_back(&acc.getButtonView());
+		buttons.push_back(&acc.getButtonEdit());
+		buttons.push_back(&acc.getButtonDelete());
+	}
+
+	for (TextButton* button : buttons)
+	{
+		if (button->isCursorOn(*g_Window))
+		{
+			button->setHighlight(true);
+		}
+		else
+		{
+			button->setHighlight(false);
+		}
+	}
+}
+
+void AccountsPageState::onMouseButtonPressed()
+{
+	if (m_NotifyViewAccount.isActive())
+	{
+		if (m_NotifyViewAccount.getButtons()[Notify_ViewAccount::OK].isCursorOn(*g_Window))
+		{
+			m_NotifyViewAccount.setActive(false);
+		}
+
+		return;
+	}
+
+	if (m_NotifyDeleteAccount.isActive())
+	{
+		auto& buttons = m_NotifyDeleteAccount.getButtons();
+
+		for (uint8_t i = 0; i < buttons.size(); i++)
+		{
+			if (buttons[i].isCursorOn(*g_Window))
+			{
+				switch (i)
+				{
+				case Notify_DeleteAccount::CONFERMA:
+				{
+					mINI::INIStructure ini;
+					DATAFILE.read(ini);
+
+					ini.remove(m_NotifyDeleteAccount.getName());
+
+					DATAFILE.write(ini);
+
+					init();
+				}
+				break;
+				case Notify_DeleteAccount::ANNULLA:
+					break;
+				}
+
+				m_NotifyDeleteAccount.setActive(false);
+			}
+		}
+
+		return;
+	}
+
+	if (m_TextBoxSearch.isCursorOn(*g_Window))
+	{
+		m_TextBoxSearch.setSelected(true);
+	}
+	else
+	{
+		m_TextBoxSearch.setSelected(false);
+	}
+
+	for (Account* acc : m_VisibleAccounts)
+	{
+		enum : uint8_t
+		{
+			MOSTRA,
+			MODIFICA,
+			ELIMINA,
+		};
+		std::unordered_map<uint8_t, TextButton*> buttons;
+
+		buttons[MOSTRA] = &acc->getButtonView();
+		buttons[MODIFICA] = &acc->getButtonEdit();
+		buttons[ELIMINA] = &acc->getButtonDelete();
+
+		for (uint8_t i = 0; i < buttons.size(); i++)
+		{
+			if (buttons[i]->isCursorOn(*g_Window))
+			{
+				switch (i)
+				{
+				case MOSTRA:
+				{
+					m_NotifyViewAccount = Notify_ViewAccount("Account: " + acc->getAccountInfo().name);
+
+					std::stringstream contents;
+					contents << "\nNome utente:\n" << acc->getAccountInfo().username << "\n\nPassword:\n" << acc->getAccountInfo().password;
+					m_NotifyViewAccount.setContents(contents.str());
+
+					m_NotifyViewAccount.setActive(true);
+				}
+				break;
+				case MODIFICA:
+					g_Machine.add(StateRef(new EditAccountState(acc->getAccountInfo())), false);
+					break;
+				case ELIMINA:
+				{
+					m_NotifyDeleteAccount = Notify_DeleteAccount(acc->getAccountInfo().name);
+
+					std::stringstream contents;
+					contents << "\nStai per eliminare:\n" << acc->getAccountInfo().name
+						<< "\n\n\n(!) Questa operazione è irreversibile.";
+					m_NotifyDeleteAccount.setContents(contents.str());
+
+					m_NotifyDeleteAccount.setActive(true);
+				}
+				break;
+				}
+			}
+		}
+	}
+
+	for (uint8_t i = 0; i < m_Buttons.size(); i++)
+	{
+		if (m_Buttons[i].isCursorOn(*g_Window))
+		{
+			switch (i)
+			{
+			case Button::AGGIUNGI:
+				g_Machine.add(StateRef(new AddAccountState()), false);
+				break;
+			case Button::INDIETRO:
+				g_Machine.remove();
+				break;
+			}
+		}
+	}
+}
+
+void AccountsPageState::onScrollPage(const sf::Event& event)
+{
+	if (m_VisibleAccounts.empty())
+	{
+		return;
+	}
+
+	const float DELTA = event.mouseWheelScroll.delta;
+
+	const int Y_MOVE = 55;
+
+	if (DELTA > 0.f)
+	{
+		for (uint16_t i = 0; i < m_VisibleAccounts.size(); i++)
+		{
+			const float Y_DEFAULT = m_Background.getSize().y + 10.f + 110.f * i;
+
+			if (m_VisibleAccounts[i]->getPosition().y != Y_DEFAULT)
+			{
+				m_VisibleAccounts[i]->setPosition({ ACCOUNT_XPOS(m_Accounts[i].getSize().x), m_VisibleAccounts[i]->getPosition().y + Y_MOVE });
+			}
+		}
+	}
+	else if (DELTA < 0.f)
+	{
+		for (uint16_t i = 0; i < m_VisibleAccounts.size(); i++)
+		{
+			const float Y_DEFAULT = m_Background.getSize().y + 10.f + 110.f * i;
+
+			if (m_VisibleAccounts[i]->getPosition().y <= Y_DEFAULT)
+			{
+				m_VisibleAccounts[i]->setPosition({ ACCOUNT_XPOS(m_Accounts[i].getSize().x), m_VisibleAccounts[i]->getPosition().y - Y_MOVE });
+			}
+		}
+	}
+}
+
+void AccountsPageState::onSearch(const std::string& search)
+{
+	m_VisibleAccounts.clear();
+
+	for (Account& acc : m_Accounts)
+	{
+		std::string name = acc.getAccountInfo().name;
+
+		if (search.length() > name.length())
+		{
+			continue;
+		}
+
+		bool found = true;
+		for (int i = 0; i < search.length(); i++)
+		{
+			if (search[i] != name[i])
+			{
+				found = false;
+				break;
+			}
+		}
+
+		if (found)
+		{
+			m_VisibleAccounts.push_back(&acc);
+		}
+	}
+
+	for (uint16_t i = 0; i < m_VisibleAccounts.size(); i++)
+	{
+		m_VisibleAccounts[i]->setPosition({ ACCOUNT_XPOS(m_Accounts[i].getSize().x), m_Background.getSize().y + 10.f + 110.f * i });
+	}
 }
 
 void AccountsPageState::loadAccounts()
